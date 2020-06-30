52b46e394f20d847da8ff79458d1e1a890124c72
jsmit42 <50594564+jsmit42@users.noreply.github.com>
2020-06-29 23:11:28 -0700

    added date conversion

--- indexGenerator.cpp
+++ indexGenerator.cpp
@@ -15,20 +15,79 @@ struct Post
     string title;
     string link;
     string date;
+    int numDate;
 };
 
 vector<Post> history;
 
+string intDateToString(int intFormat);
 void readIndex();
 void writeIndex();
 void writeToHTML();
 string getHTMLFormat(Post input);
 string getIndexFormat(Post input);
 
+string intDateToString(int intFormat)
+{
+    string day, month, year;
+    day = to_string(intFormat % 100);
+    intFormat /= 100;
+
+    //find month
+    switch (intFormat % 100)
+    {
+    case 1:
+        month = "January";
+        break;
+    case 2:
+        month = "February";
+        break;
+    case 3:
+        month = "March";
+        break;
+    case 4:
+        month = "April";
+        break;
+    case 5:
+        month = "May";
+        break;
+    case 6:
+        month = "June";
+        break;
+    case 7:
+        month = "July";
+        break;
+    case 8:
+        month = "August";
+        break;
+    case 9:
+        month = "September";
+        break;
+    case 10:
+        month = "October";
+        break;
+    case 11:
+        month = "November";
+        break;
+    case 12:
+        month = "December";
+        break;
+    
+    default:
+        month = "INVALID MONTH";
+        break;
+    }
+    intFormat /= 100;
+
+    //get year
+    year = "20" + to_string(intFormat % 100);
+
+    return month + " " + day + ", " + year;
+}
 
 string getIndexFormat(Post input)
 {
-    return input.date + "|" + input.title + "|" + input.link;
+    return input.numDate + "|" + input.title + "|" + input.link;
 }
 
 string getHTMLFormat(Post input)
